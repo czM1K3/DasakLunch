d21cd34746816ea2ee2c6374cb0357532dd31a68
czM1K3 <misamadera1@gmail.com>
2021-10-01 20:09:00 +0200

    Added supabase to main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  String supabaseUrl = dotenv.get("SUPABASE_URL");
+  String supabaseAnonKey = dotenv.get("SUPABASE_ANON_KEY");
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
+  );
   runApp(const MyApp());
 }
 
